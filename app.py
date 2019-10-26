6249513250d4aa1b31d8785d83a47af6cf32f330
ethan-w-roland <ethan.w.roland@gmail.com>
2019-10-26 16:17:55 -0400

    test

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, make_response, jsonify
+import random
 
 # initialize the flask app
 app = Flask(__name__)
@@ -14,21 +15,20 @@ def redirect():
     req = request.get_json(force=True)
     intent = req["queryResult"]["intent"]["displayName"]
 
-    if intent == "DetectIntent":
+    if intent == "DetectIntent": #aka which sub-app to go to
         helpVariable = req["queryResult"]["parameters"]["HelpCategory"]
-
         return HandleDetectIntent(helpVariable)
 
-    elif intent == "GetSpeechTopic":
+    elif intent == "GetSpeechTopic":  #aka set speech topic
         speechTopic = req["queryResult"]["parameters"]["SpeechTopic"]
         return SetSpeechTopic(speechTopic)
 
-    elif intent == "GetSpeech":
+    elif intent == "GetSpeech": #aka get speech transcript
         Transcript = req["queryResult"]["parameters"]["Transcript"]
         print(Transcript)
-
-    # return a fulfillment response
-    #return {'fulfillmentText': 'This is a response from webhook.'}
+        return {'fulfillmentText': 'This is a response from webhook.'}
+    else:
+        return {'fulfillmentText': 'This is a response from webhook.'}
 
 def HandleDetectIntent(HelpVariable:  str):
     if HelpVariable == "speech":
@@ -37,12 +37,18 @@ def HandleDetectIntent(HelpVariable:  str):
                 "name" : "AskSpeechTopicEvent"
             }
         }
-    else:
-        return {'fulfillmentText': 'This is a response from webhook.'}
+    elif HelpVariable == 'interview':
+        #fetch random interview topic
+        lines = open('./interview/questions.txt').read().splitlines()
+        intTopic = random.choice(lines).strip()
+        print('Randomly picked interview topic is: ', intTopic)
+        return {'fulfillmentText': intTopic}
+
 def SetSpeechTopic(speechTopic: str):
     SpeechTopic = speechTopic
-    print("Topic was set to,",SpeechTopic)
+    print("Topic was set to: ", SpeechTopic)
     return {'fulfillmentText': 'Great begin your speech.'}
+
 # create a route for webhook
 @app.route('/webhook', methods= ['POST'])
 def webhook():
